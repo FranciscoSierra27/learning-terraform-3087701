d7a93b3f5cb1aa2bd0e0a81cea4d58bd0436e1a7
Francisco Sierra <franciscosierraperez27@gmail.com>
2025-04-30 09:54:29 -0600

    Add module for security groups

--- main.tf
+++ main.tf
@@ -18,7 +18,22 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
+  vpc_security_group_id = [module.blog_sg.security_group_id]
+
   tags = {
-    Name = "HelloWorld"
+    Name = "Learning Terraform"
   }
+}
+
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.3.0"
+  name    = "blog"
+
+  vpc_id             = data.aws_vpc.default.id
+  ingress_rules      = ["http-80-tcp","https-430-tcp"]
+  ingress_cidr_block = ["0.0.0.0/0"]
+
+  egress_rules      = ["all-all"]
+  egress_cidr_block = ["0.0.0.0/0"]
 }
