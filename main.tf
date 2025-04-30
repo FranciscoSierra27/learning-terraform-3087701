239114020909e9da794787d1163d0239e169fade
Francisco Sierra <franciscosierraperez27@gmail.com>
2025-04-30 09:59:53 -0600

    Fix more typos and add default vpc block

--- main.tf
+++ main.tf
@@ -14,11 +14,15 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_id = [module.blog_sg.security_group_id]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
     Name = "Learning Terraform"
